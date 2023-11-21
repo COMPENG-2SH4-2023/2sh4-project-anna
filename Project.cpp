0bffeb23091887b598a3d6a40eb7c05497077d87
norria1 <norria1@mcmaster.ca>
2023-11-21 13:25:05 -0500

    Drew border

--- Project.cpp
+++ Project.cpp
@@ -57,6 +57,30 @@ void RunLogic(void)
 void DrawScreen(void)
 {
     MacUILib_clearScreen();    
+    char matrix[10][20];
+    for(int i = 0; i < 10; i++)
+    {
+	    for(int j = 0; j < 20; j++)
+	    {
+		    if(i == 0 || i == 9 || j == 0 || j == 19)
+		    {
+			    matrix[i][j] = '#';
+		    }
+		    else
+		    {
+			    matrix[i][j] = ' ';
+		    }
+	    }
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+	    for(int j = 0; j < 20; j++)
+	    {
+		    MacUILib_printf("%c", matrix[i][j]);
+	    }
+	    MacUILib_printf("\n");
+    }
 
 }
 
