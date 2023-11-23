8e69204cedfcc6ca2a6f8919ae572d339c15e2d4
Angelique Stahl <stahla@mcmaster.ca>
2023-11-23 16:08:07 -0500

    Iteration 2A

--- Project.cpp
+++ Project.cpp
@@ -97,7 +97,7 @@ void DrawScreen(void)
     MacUILib_clearScreen();    
     //recall: there are 10 rows, i.e. i-positions or movements on the y-axis, and 20 columns, i.e. j-positions or movements on the x-axis.
     char matrix[gameMechanics->getBoardSizeY()][gameMechanics->getBoardSizeX()];
-    objPos test = objPos(4, 3, '!'); //test object with x-pos=4, y-pos =3, symbol of '!'
+    //objPos test = objPos(4, 3, '!'); //test object with x-pos=4, y-pos =3, symbol of '!'
     objPos tempPos; // temporarory position of Player Object
     playerObject -> getPlayerPos(tempPos);
     for(int i = 0; i < gameMechanics->getBoardSizeY(); i++)
@@ -112,10 +112,12 @@ void DrawScreen(void)
             {
                 matrix[i][j] = tempPos.symbol;
             }
-		    else if(j == test.x && i == test.y)
+		    /*
+			else if(j == test.x && i == test.y)
 		    {
 			    matrix[i][j] = test.symbol;
 		    }
+			*/
 		    else
 		    {
 			    matrix[i][j] = ' ';
--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -3,3 +3,69 @@
 // Paste your CUTE Tested implementation here.
 // Paste your CUTE Tested implementation here.
 // Paste your CUTE Tested implementation here.
+objPosArrayList::objPosArrayList()
+{
+    sizeList = 0;
+    sizeArray = ARRAY_MAX_CAP;
+    aList = new objPos[ARRAY_MAX_CAP];
+}
+
+objPosArrayList::~objPosArrayList()
+{
+    delete[] aList;
+}
+
+int objPosArrayList::getSize()
+{
+    return sizeList;
+}
+
+void objPosArrayList::insertHead(objPos thisPos)
+{
+    for(int i=sizeList; i>0; i--)
+    {
+        aList[i].setObjPos(aList[i-1]);
+    }
+    aList[0].setObjPos(thisPos); // sets first element to head obj
+    sizeList++;
+}
+
+void objPosArrayList::insertTail(objPos thisPos)
+{
+    if(sizeList == sizeArray) //FULL
+    {
+        return;
+    }
+    aList[sizeList].setObjPos(thisPos);
+    sizeList++;
+}
+
+void objPosArrayList::removeHead()
+{
+    for(int i=0; i<=sizeList;i++)
+    {
+        aList[i].setObjPos(aList[i+1]);
+    }
+    sizeList--;
+}
+
+void objPosArrayList::removeTail()
+{
+    aList[sizeList].setObjPos(0,0,0);
+    sizeList--;
+}
+
+void objPosArrayList::getHeadElement(objPos &returnPos)
+{
+    aList[0].getObjPos(returnPos);
+}
+
+void objPosArrayList::getTailElement(objPos &returnPos)
+{
+    aList[sizeList-1].getObjPos(returnPos);
+}
+
+void objPosArrayList::getElement(objPos &returnPos, int index)
+{
+    aList[index].getObjPos(returnPos);
+}
